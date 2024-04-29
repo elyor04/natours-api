36b7580c1c0ed998c15f58a91c85bb2cbbdccc0b
elyor04 <tuxtamuratovelyor@gmail.com>
2024-04-29 17:02:56 +0500

    Just commit

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 const app = require("./app");
+const port = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Database connected successfully!"))
   .catch(() => console.log("Failed to connect to database!"));
-
-const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
