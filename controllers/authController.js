808251565ad1d3ef910fdc6c1d671ce5028fa54c
elyor04 <tuxtamuratovelyor@gmail.com>
2024-05-01 11:13:01 +0500

    Just commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,7 @@ exports.signup = catchAsync(async function (req, res, next) {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    photo: req.body.photo,
   });
 
   res.status(201).json({
