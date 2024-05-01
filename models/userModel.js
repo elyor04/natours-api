38f33ffa7ba033741365eb98160b5a00eff95d31
elyor04 <tuxtamuratovelyor@gmail.com>
2024-05-01 11:09:17 +0500

    Just commit

--- models/userModel.js
+++ models/userModel.js
@@ -34,6 +34,10 @@ const userSchema = new mongoose.Schema({
   __v: { type: Number, select: false },
 });
 
+userSchema.methods.checkPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
 userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 12);
@@ -41,12 +45,5 @@ userSchema.pre("save", async function (next) {
   }
   next();
 });
-
-userSchema.methods.checkPassword = async function (
-  candidatePassword,
-  userPassword
-) {
-  return await bcrypt.compare(candidatePassword, userPassword);
-};
 
 module.exports = mongoose.model("User", userSchema);
