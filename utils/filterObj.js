e0cfaf1e915c3c3550aa017b2ded38ee838e016f
elyor04 <tuxtamuratovelyor@gmail.com>
2024-05-08 17:33:05 +0500

    Just commit

--- utils/filterObj.js
+++ utils/filterObj.js
@@ -1,8 +1,8 @@
 function filterObj(obj, ...keys) {
   const newObj = {};
-  for (const key in obj) {
-    if (keys.includes(key)) newObj[key] = obj[key];
-  }
+  keys.forEach((key) => {
+    if (key in obj) newObj[key] = obj[key];
+  });
   return newObj;
 }
 
